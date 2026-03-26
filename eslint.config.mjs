6a09bf1d158b3f510557f54f0d93d2f01f77bd8f
deprecatum <deprecatum@gmail.com>
2026-03-26 19:09:34 +0000

    Removed nextjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,17 @@
 import storybook from "eslint-plugin-storybook";
 
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import configs from "eslint-config-webpack";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...configs,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
+    // ".next/**",
+    // "next-env.d.ts",
     "out/**",
     "build/**",
-    "next-env.d.ts",
   ]),
   ...storybook.configs["flat/recommended"]
 ]);
