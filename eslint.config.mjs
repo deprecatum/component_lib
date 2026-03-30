3095d2d6728c485cd0086be1a144c3b9a40c490a
deprecatum <deprecatum@gmail.com>
2026-03-30 18:03:44 +0100

    Tweaked eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginStorybook from "eslint-plugin-storybook";
 import json from "@eslint/json";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
@@ -10,4 +11,5 @@ export default defineConfig([
   { files: ["src/**/*.{ts,tsx}"], plugins: { js, pluginReact, tseslint }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["src/**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["src/**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { files: ["src/**/*.stories.{ts,tsx}"], plugins: { pluginStorybook } },
 ]);
