277f760f653664bae00e5e66c34af2053e9cf5b7
deprecatum <deprecatum@gmail.com>
2026-03-31 15:41:58 +0100

    Addressing css modules import

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,38 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
+  "framework": "@storybook/react-webpack5",
   "stories": [
     "../src/**/*.stories.@(ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-webpack5-compiler-babel",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    {
+      name: '@storybook/addon-styling-webpack',
+      options: {
+        rules: [
+          // Replaces existing CSS rules to support CSS Modules
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: {
+                    auto: true,
+                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                  },
+                  esModule: false,
+                },
+              }
+            ],
+          }
+        ]
+      }
+    }
   ],
-  "framework": "@storybook/react-webpack5",
+  
 };
 export default config;
